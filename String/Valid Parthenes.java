35f4d1a9df9de0ae0f2be77b845dcf81a05a9328
codewithpriya23 <priyaverma2723@gmail.com>
2026-03-30 21:10:58 +0530

    Added valid parethenes

---
+++ String/Valid Parthenes.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st=new Stack<>();
+     for(int i=0; i<s.length(); i++){
+        char ch=s.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+                continue;
+            }
+            if (st.isEmpty()) return false;
+            if (ch == ')' && st.peek() == '(') {
+                st.pop();
+            } 
+            else if (ch == '}' && st.peek() == '{') {
+                st.pop();
+            } 
+            else if (ch == ']' && st.peek() == '[') {
+                st.pop();
+            } 
+            else {
+                return false;
+            }
+        }
+        return st.isEmpty();
+    }
+    }
+        
