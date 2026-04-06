29ff4ce3774f037f2071e8c7a91af192f82bb3c6
codewithpriya23 <priyaverma2723@gmail.com>
2026-04-06 23:20:55 +0530

    Added majority element

---
+++ Arrays/MajorityElement.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int majorityElement(int[] nums){
+        int winner=nums[0];
+        int vote=1;
+        for(int i=1; i<nums.length; i++){
+            if(nums[i]==winner){
+                vote++;
+            }else{
+                vote--;
+            }
+            if(vote==0){
+                winner=nums[i];
+                vote=1;
+
+            }
+        }
+        return winner;
+    }
+}
