a84585b9a741ae46babb21ee0684bc46a077139b
codewithpriya23 <priyaverma2723@gmail.com>
2026-04-20 22:26:08 +0530

    Added max profit

---
+++ Arrays/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minprice=prices[0];
+        int maxProfit=0;
+        for(int i=1; i<prices.length; i++){
+            if(prices[i]<minprice){
+                minprice=prices[i];
+            }else{
+                int profit=prices[i]-minprice;
+               
+              maxProfit=Math.max(maxProfit,profit);
+            }
+        }
+            return maxProfit;
+    }
+}
